a6d942adc66e04a7e96dc99a34389bbd77bdf622
SahirMalkani <41510686+SahirMalkani@users.noreply.github.com>
2023-12-29 19:13:48 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,47 @@
+// import "./App.css";
+import { Fstcomp0 } from "./components/fstcomp";
+import { Dusra } from "./components/dusra";
+
+import { Teesra } from "./components/teesra";
+
+import { Chhotha } from "./components/chhotha";
+import {Bbb} from "./components/Navbar"
+
+
+import { Panchwa } from "./components/Panchwa";
+import { Chhata } from "./components/Chhata";
+import { Saatwa } from "./components/Saatwa";
+
+const App= () =>{
+
+  const name="Sahir"
+
+  return(
+  <>
+<header>< Bbb /></header>
+  
+
+  <main>
+  < Fstcomp0 />
+
+  <Dusra />
+  <Teesra />
+<Chhotha />
+  
+<Panchwa />
+
+<Chhata />
+< Fstcomp0 />
+</main>
+
+
+<footer><Saatwa /></footer>
+
+
+  
+  </>
+
+);
+}
+
+export default App;
